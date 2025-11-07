5f236d5bf5d6a38a537b38b04f4aa68efef8577a
Sedjro Chancel Castole Agossou Yao <leroi.chancel@gmail.com>
2025-11-07 07:13:33 +0000

    fix: Remove invalid default role for User model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SchoolsModule } from './schools/schools.module';
+import { PrismaService } from './prisma.service'; // On importe notre service
 
 @Module({
-  imports: [],
+  imports: [SchoolsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    PrismaService, // On le fournit ici pour qu'il soit injectable partout
+  ],
 })
 export class AppModule {}
